6fb0fc9a240858066bc4d87dd69a4702fb2ce1ad
psam20 <unique.kishan.gupta@gmail.com>
2023-03-05 23:10:05 +0545

    Completed Http Request

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
-console.log('Starting')
-
-setTimeout(()=>{
-    console.log('2 Second Timer')
-},2000)
-
-setTimeout(()=>{
-    console.log('0 Second Timer')
-}, 0)
-
-console.log('Stopping')
+const request = require('request');
+const url ='http://api.weatherstack.com/current?access_key=31b024576c07a4d156cb233b25fb4c91&query=40.714,-74.006'
+request({url :url} ,(err,response)=>{
+    const data = JSON.parse(response.body)
+ console.log(data.current)
+})
